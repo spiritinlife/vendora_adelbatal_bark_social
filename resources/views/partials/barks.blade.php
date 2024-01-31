34c59ab3fbe5b2217c032ee7911ebdb3263851c8
adel-batal <batal.adel24@gmail.com>
2024-02-01 00:08:35 +0200

    Remove !! directives to avoid xss attacks

--- resources/views/partials/barks.blade.php
+++ resources/views/partials/barks.blade.php
@@ -3,7 +3,7 @@
         @if($feedType === 'friends')
             <a href="/users/{{$bark->user->id}}">{{$bark->user->name}} barked:</a>
         @endif
-        <p>{!! $bark->message !!}</p>
+        <p>{{ $bark->message }}</p>
         <small>{{ $bark->created_at }}</small>
     </article>
     <hr>
